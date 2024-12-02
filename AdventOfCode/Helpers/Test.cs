4414d05009eb05507d77605e9260e9b8f534328a
Nathan Boshoff <dc.shephard@gmail.com>
2024-12-02 09:48:37 +0000

    chore: add test case helper

---
+++ AdventOfCode/Helpers/Test.cs
@@ -0,0 +1,29 @@
+﻿
+namespace AdventOfCode
+{
+    class Test
+    {
+        public delegate string SolutionFunction(string input);
+
+        public static IEnumerable<string> Run(SolutionFunction SolutionFunction, List<(string TestInput, string ExpectedSolve1, string ExpectedSolve2)> _testCases, int part)
+        {
+            return _testCases.Select((test, index) =>
+            {
+                string actual = SolutionFunction(test.TestInput);
+                return ValidateResult(actual, part == 1 ? test.ExpectedSolve1 : test.ExpectedSolve2, $"Part {part} Test {index + 1}");
+            });
+        }
+
+        private static string ValidateResult(string actual, string expected, string testName)
+        {
+            if (actual == expected)
+            {
+                return $"[PASS] {testName}";
+            }
+            else
+            {
+                return $"[FAIL] {testName}. Expected: {expected}, Actual: {actual}.";
+            }
+        }
+    }
+}
