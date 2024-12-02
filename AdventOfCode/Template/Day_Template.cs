fc6930590f8651791b47ee181a604d6b37de1621
Nathan Boshoff <dc.shephard@gmail.com>
2024-12-02 09:48:19 +0000

    chore: update template

---
+++ AdventOfCode/Template/Day_Template.cs
@@ -0,0 +1,65 @@
+﻿/*namespace AdventOfCode;
+
+public class DayXX : BaseDay
+{
+    private readonly string _input;
+    private readonly List<(string TestInput, string ExpectedSolve1, string ExpectedSolve2)> _testCases;
+
+    public DayXX()
+    {
+        _input = File.ReadAllText(InputFilePath);
+
+        _testCases =
+        [
+            (
+                """
+                
+                """,
+                "1", // Expected result for part 1
+                "2"  // Expected result for part 2
+            )
+        ];
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var testResults = Test.Run(Solve_1_Initial, _testCases, 1).ToList();
+        var solution = Solve_1_Initial(_input);
+
+        return new($"{string.Join("\n", testResults)}\nSolution to {ClassPrefix} {CalculateIndex()}, part 1: {solution}");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var testResults = Test.Run(Solve_2_Initial, _testCases, 2).ToList();
+        var solution = Solve_2_Initial(_input);
+
+        return new($"{string.Join("\n", testResults)}\nSolution to {ClassPrefix} {CalculateIndex()}, part 2: {solution}");
+    }
+
+    public string Solve_1_Initial(string input)
+    {
+        var solution = 1;
+
+        return $"{solution}";
+    }
+
+    public string Solve_2_Initial(string input)
+    {        
+        var solution = 2;
+        return $"{solution}";
+    }
+
+    public string Solve_1_Optimized(string input)
+    {
+        var solution = 0;
+        return $"{solution}";
+    }
+
+    public string Solve_2_Optimized(string input)
+    {
+        var solution = 0;
+        return $"{solution}";
+    }
+}
+*/
