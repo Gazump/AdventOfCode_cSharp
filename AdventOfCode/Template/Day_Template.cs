a7c79da32f9ce81112be1c8a2a362066bc037e74
Nathan Boshoff <dc.shephard@gmail.com>
2024-12-02 11:35:24 +0000

    chore: update template with optimized inputs

--- AdventOfCode/Template/Day_Template.cs
+++ AdventOfCode/Template/Day_Template.cs
@@ -1,9 +1,13 @@
-﻿/*namespace AdventOfCode;
+﻿/*using static AdventOfCode.Helpers.Test;
+using static AdventOfCode.Helpers.InputHandler;
+
+namespace AdventOfCode;
 
 public class DayXX : BaseDay
 {
     private readonly string _input;
     private readonly List<(string TestInput, string ExpectedSolve1, string ExpectedSolve2)> _testCases;
+    private readonly bool _runTestCases = true;
 
     public DayXX()
     {
@@ -23,43 +27,66 @@ public class DayXX : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        var testResults = Test.Run(Solve_1_Initial, _testCases, 1).ToList();
-        var solution = Solve_1_Initial(_input);
+        if (_runTestCases)
+        {
+            var testResults = RunTestCases(Solve_1_Initial, _testCases, 1).ToList();
+            var solution = Solve_1_Initial(_input);
 
-        return new($"{string.Join("\n", testResults)}\nSolution to {ClassPrefix} {CalculateIndex()}, part 1: {solution}");
+            return new($"{string.Join("\n", testResults)}\nSolution to {ClassPrefix} {CalculateIndex()}, part 1: {solution}");
+        }
+        else
+        {
+            var solution = Solve_1_Optimized(_input);
+
+            return new($"Optimized Solution: {solution}");
+        }
     }
 
     public override ValueTask<string> Solve_2()
     {
-        var testResults = Test.Run(Solve_2_Initial, _testCases, 2).ToList();
-        var solution = Solve_2_Initial(_input);
+        if (_runTestCases)
+        {
+            var testResults = RunTestCases(Solve_2_Initial, _testCases, 2).ToList();
+            var solution = Solve_2_Initial(_input);
 
-        return new($"{string.Join("\n", testResults)}\nSolution to {ClassPrefix} {CalculateIndex()}, part 2: {solution}");
+            return new($"{string.Join("\n", testResults)}\nSolution to {ClassPrefix} {CalculateIndex()}, part 2: {solution}");
+        }
+        else
+        {
+            var solution = Solve_2_Optimized(_input);
+
+            return new($"Optimized Solution: {solution}");
+        }
     }
 
     public string Solve_1_Initial(string input)
     {
         var solution = 1;
+        var inputSpan = input.AsSpan();
+
+        while (!inputSpan.IsEmpty)
+        {
+            var line = ReadNextLine(ref inputSpan);
+            if (line.IsEmpty) continue;
+            
+        }        
 
         return $"{solution}";
     }
 
     public string Solve_2_Initial(string input)
     {        
-        var solution = 2;
-        return $"{solution}";
+        return $"No Solution";
     }
 
     public string Solve_1_Optimized(string input)
     {
-        var solution = 0;
-        return $"{solution}";
+        return $"No Solution";
     }
 
     public string Solve_2_Optimized(string input)
     {
-        var solution = 0;
-        return $"{solution}";
+        return $"No Solution";
     }
 }
 */
